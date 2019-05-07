91b4fc4be6ab7a3a563fd5c23138fcc1a3cb64ee
wedtlcy <wedtlcy@126.com>
2019-05-07 19:23:46 +0800

    add jackson test case

--- src/main/java/com/xgdfin/exercise/json/JacksonTest.java
+++ src/main/java/com/xgdfin/exercise/json/JacksonTest.java
@@ -5,15 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xgdfin.exercise.bean.User;
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author chuanyin.li
- * @create 2019-04-29 14:48
- **/
-public class JacksonTest {
+
+/** 
+* @description:  Jackson工具使用
+* @author: chuanyin.li
+* @date: 2019/5/7 
+*/ 
+public class JacksonTest implements Serializable {
 
     private ObjectMapper mapper = new ObjectMapper();
 
@@ -25,8 +28,15 @@ public class JacksonTest {
      * writeValueAsBytes(Object arg0)把arg0转成json序列，并把结果输出成字节数组。
      * writeValueAsString(Object arg0)把arg0转成json序列，并把结果输出成字符串。
      */
+    /**
+     * @description:
+     * @param: []
+     * @return: void
+     * @author: chuanyin.li
+     * @date: 2019/5/7
+     */
     @Test
-    public void javaObj2Json() throws Exception{
+    public void javaObj2Json() throws Exception {
 
         User user = new User();
         user.setName("zhangsan");
@@ -41,6 +51,24 @@ public class JacksonTest {
         String json = mapper.writeValueAsString(user);
         System.out.println(json);
 
+    }
+
+    /**
+     * @description: Java集合转JSON
+     * @param: []
+     * @return: void
+     * @author: chuanyin.li
+     * @date: 2019/5/7
+     */
+    @Test
+    public void javaSet2Json() throws Exception {
+        User user = new User();
+        user.setName("zhangsan");
+        user.setEmail("zhangsan@163.com");
+        user.setAge(20);
+
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd");
+        user.setBirthday(dateformat.parse("1996-10-01"));
         //Java集合转JSON
         //输出结果：[{"name":"zhangsan","age":20,"birthday":844099200000,"email":"zhangsan@163.com"}]
         List<User> users = new ArrayList<User>();
@@ -49,12 +77,34 @@ public class JacksonTest {
         System.out.println(jsonlist);
     }
 
-
+    /**
+     * @description: JSON反序列化
+     * @param: []
+     * @return: void
+     * @author: chuanyin.li
+     * @date: 2019/5/7
+     */
     @Test
-    public void json2List() throws Exception{
-        String json = "[{\"name\":\"zhangsan\",\"age\":20,\"birthday\":844099200000,\"email\":\"zhangsan@163.com\"}]";
-        List<User> beanList = mapper.readValue(json, new TypeReference<List<User>>() {});
-        System.out.println(beanList);
+    public void jsonDeSerializable() throws Exception {
+        String json = "{\"name\":\"zhangsan\",\"age\":20,\"birthday\":844099200000,\"email\":\"zhangsan@163.com\"}";
+        /**
+         * ObjectMapper支持从byte[]、File、InputStream、字符串等数据的JSON反序列化。
+         */
+        User user = mapper.readValue(json, User.class);
+        System.out.println(user);
     }
 
+    /**
+     * @description: JSON2List集合
+     * @param: []
+     * @return: void
+     * @author: chuanyin.li
+     * @date: 2019/5/7
+     */
+    @Test
+    public void json2List() throws Exception {
+        String json = "[{\"name\":\"zhangsan\",\"age\":20,\"birthday\":844099200000,\"email\":\"zhangsan@163.com\"}]";
+        List<User> beanList = mapper.readValue(json, new TypeReference<List<User>>() { });
+        System.out.println(beanList);
+    }
 }
