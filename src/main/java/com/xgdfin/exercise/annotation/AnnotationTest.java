2c41e7ae0d710a67618ae5938d68f9f1e792e53f
wedtlcy <wedtlcy@126.com>
2019-05-08 15:37:01 +0800

    update AnnotationTest test case

--- src/main/java/com/xgdfin/exercise/annotation/AnnotationTest.java
+++ src/main/java/com/xgdfin/exercise/annotation/AnnotationTest.java
@@ -9,14 +9,20 @@ import java.lang.reflect.Field;
 /**
  * 自定义注解解析
  *
- * @author chuanyin.li
- * @create 2019-05-08 15:02
+ * @author: chuanyin.li
+ * @date: 2019/5/8
  **/
 public class AnnotationTest {
 
-
+    /** 
+    * @description:  解析自定义注解
+    * @param: [] 
+    * @return: void 
+    * @author: chuanyin.li
+    * @date: 2019/5/8
+    */ 
     @Test
-    public void analysisAnnotationTest(){
+    public void analysisAnnotationTest() {
         Field[] declaredFields = User.class.getDeclaredFields();
         for (int i = 0; i < declaredFields.length; i++) {
             Field declaredField = declaredFields[i];
@@ -26,17 +32,16 @@ public class AnnotationTest {
                 continue;
             }
 
+            //获取注解描述
             String desc = annotation.desc();
-
+            //获取长度
             String maxLength = annotation.maxLength();
-
+            //获取最小长度
             String minLength = annotation.minLength();
-
+            //获取是否为空
             boolean nonNull = annotation.nonNull();
 
-            Class type = annotation.type();
-
-            System.out.println(String.format("fieldName=%s, desc=%s, maxLength=%s, minLength=%s, nonNull=%s, type=%s",fieldName,desc,maxLength,minLength,nonNull,type));
+            System.out.println(String.format("fieldName=%s, desc=%s, maxLength=%s, minLength=%s, nonNull=%s, type=%s", fieldName, desc, maxLength, minLength, nonNull));
 
 
         }
