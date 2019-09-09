93d826f994d44f70eeaaa83c50b367f3e35f057e
wedtlcy <wedtlcy@126.com>
2019-09-10 07:01:01 +0800

    stream api

---
+++ src/main/java/com/xgdfin/exercise/java8/stream/demo2.java
@@ -0,0 +1,35 @@
+package com.xgdfin.exercise.java8.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 筛选元素
+ *
+ * filter 使用
+ * distinct 使用
+ * limit 使用
+ * skip 使用
+ *
+ * @author chuanyin.li
+ * @date 2019-09-10
+ */
+public class demo2 {
+
+    public static void main(String[] args) {
+        List<Project> projects = Project.buildData();
+
+        List<Project> collect = projects.stream()
+                .filter(project -> project.getBugs() > 100)
+                .collect(Collectors.toList());
+        collect.forEach(e-> System.out.println(e));
+
+        // distinct
+        Stream<Integer> numbers = Stream.of(1, 2, 3, 3, 2, 4);
+        numbers.distinct().limit(3).forEach(n -> System.out.println(n));
+
+        System.out.println("===================");
+        Stream.of(1, 2, 3, 3, 2, 4).skip(4).forEach(n -> System.out.println(n));
+    }
+}
